80daa7db80f2982712f67573c2b6f05ed0249675
fifi-PC\fifi <puddingcoco711@gmail.com>
2018-09-30 18:16:12 +0800

    9/30  UI、場景切換

---
+++ Noir/Assets/UI/Script/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public void PlayGame()
+    {
+        Debug.Log("Play game");
+        SceneManager.LoadScene(2);
+    }
+    public void ChooseLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+   
+}
