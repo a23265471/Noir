b745b305accd543cd2770e2ad0cc7b5dd27b763b
3F-09\Intern <a23265471@gmail.com>
2018-10-09 18:55:16 +0800

    10/9 UI動畫

---
+++ Noir/Assets/UI/Script/UIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+    public GameObject openESCPanel;
+    private Animator ESCPanel_anim;
+    private bool ESC_PanelActive;
+    void Start()
+    {
+        openESCPanel.SetActive(false);
+        ESC_PanelActive = false;
+        ESCPanel_anim = openESCPanel.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (ESC_PanelActive)
+            {
+                ESC_PanelActive = false;
+                openESCPanel.SetActive(false);
+                
+            }
+            else
+            {
+                ESC_PanelActive = true;
+                openESCPanel.SetActive(true);
+               
+            }
+            
+        }
+        
+    }
+    public void OpenIntro()
+    {
+
+    }
+}
+
