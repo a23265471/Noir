9df8fbeed351222e5cb0379a9f92571efb5dc983
fifi-PC\fifi <puddingcoco711@gmail.com>
2018-10-11 18:59:57 +0800

    10/11  UIBug修正、動畫OK、差換頁

--- Noir/Assets/UI/Script/UIController.cs
+++ Noir/Assets/UI/Script/UIController.cs
@@ -6,10 +6,10 @@ public class UIController : MonoBehaviour {
     public GameObject ESCPanel;
     public GameObject IntroPanel;
     public GameObject Options;
-
+    //----------------------------------
     private Animator ESCPanel_Anim;
     public Animator IntroPanel_Anim;
-
+    //----------------------------------
     private bool ESCPanel_Active;
     void Start()
     {
@@ -28,11 +28,12 @@ public class UIController : MonoBehaviour {
                 ESCPanel_Active = false;
                 ESCPanel.SetActive(false);
                 ESCPanel_Anim.SetBool("OpenESCPanel", false);
-
+                IntroPanel.SetActive(false);
 
             }
             else
             {
+                Options.SetActive(true);
                 ESCPanel_Active = true;
                 ESCPanel.SetActive(true);
                 ESCPanel_Anim.SetBool("OpenESCPanel", true);
@@ -47,14 +48,16 @@ public class UIController : MonoBehaviour {
        
         Options.SetActive(false);
         IntroPanel.SetActive (true);
-        IntroPanel_Anim.SetBool("OpenIntroPanel",true);
+        IntroPanel_Anim.SetTrigger("OpenIntroPanel");
+        
      }
     public void CloseIntro()
     {
-        IntroPanel_Anim.SetBool("OpenIntroPanel", false);
+       
         Options.SetActive(true);
         IntroPanel.SetActive(false);
        
     }
+   
 }
 
