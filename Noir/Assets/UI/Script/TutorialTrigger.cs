4b5c03f26f48c428980d0673ef0f744e2043734e
fifi-PC\fifi <puddingcoco711@gmail.com>
2018-10-16 23:11:30 +0800

    10/16  關卡介面換圖OK、教學待測試

---
+++ Noir/Assets/UI/Script/TutorialTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour {
+    public GameObject Tutorial;
+	// Use this for initialization
+	void Start () {
+        Tutorial.SetActive(false);
+	}
+    private void OnTriggerOpen()
+    {
+        Tutorial.SetActive(true);
+        StartCoroutine(OnTriggerClose());
+        Tutorial.SetActive(false);
+
+    }
+    IEnumerator OnTriggerClose()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+}
