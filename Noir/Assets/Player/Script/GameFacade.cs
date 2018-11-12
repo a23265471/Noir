83c2a06fb17f1db7368df1f86f86feab505f93dd
PC-20180311YEZK\user <a23265471@gmail.com>
2018-11-12 11:47:08 +0800

    11/12 player程式修改

---
+++ Noir/Assets/Player/Script/GameFacade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(playerController))]
+
+public class GameFacade : MonoBehaviour
+{
+    private static GameFacade instance;
+    public static GameFacade GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType<GameFacade>();
+            if (instance == null)
+            {
+                throw new System.Exception("GameFacade不存在於場景中，請在場景中添加");
+            }
+            instance.Initialize();
+        }
+        return instance;
+    }
+
+    #region Controller
+    public playerController Playercontroller { private set; get; }
+    #endregion
+
+    #region Model
+    public PlayerDataCollection PlayerDatas;
+
+    #endregion
+
+    private void Initialize()
+    {
+        Playercontroller = GetComponent<playerController>();
+
+    }
+    private void Awake()
+    {
+        GetInstance();
+    }
+}
---
+++ Noir/Assets/Player/Script/Model/PlayerData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+    [System.Serializable]
+    public struct MoveParameter
+    {
+        public float RunSpeed;
+        public float FastRunSpeed;
+        public float RotateSpeed;
+    }
+    [System.Serializable]
+    public struct AvoidParameter
+    {
+        public float AvoidSpeed;
+        public float AvoidDistance;
+    }
+    [System.Serializable]
+    public struct DamageParameter
+    {
+        public float GetupTime;
+        public AudioClip AudioClip_Damage;
+    }
+    [System.Serializable]
+    public struct DieParameter
+    {
+        public AudioClip AudioClip_Damage;
+    }
+    [System.Serializable]
+    public struct AttackParameter
+    {
+        public int Combo;
+        public float AttackValue;
+        public AudioClip AudioClip_Attack;
+        public GameObject Particle_Attack;
+        public Vector3 ParticleDistanceFormPlayer;
+    }
+    [System.Serializable]
+    public struct PlayerParameter
+    {
+        public MoveParameter moveParameter;
+        public AvoidParameter avoidParameter;
+        public DamageParameter damageParameter;
+        public DieParameter dieParameter;
+        public AttackParameter[] normalAttack;
+        public AttackParameter[] specialAttack;
+        public AttackParameter[] changeAttack;
+
+    }
+
+    public PlayerParameter playerParameter;
+}
