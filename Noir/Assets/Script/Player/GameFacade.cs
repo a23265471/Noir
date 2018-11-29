3f385ebeeaed0445c5d1abbd9da7f6c2d534b00f
DESKTOP-OFKRGI3\magic <puddingcoco711@gmail.com>
2018-11-29 17:16:41 +0800

    1129  整合、資料夾整理

---
+++ Noir/Assets/Script/Player/GameFacade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[RequireComponent(typeof(playerController))]
+
+public class GameFacade : MonoBehaviour
+{
+    private static GameFacade instance;
+    public static GameFacade GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType<GameFacade>();
+            if (instance == null)
+            {
+                throw new System.Exception("GameFacade不存在於場景中，請在場景中添加");
+            }
+            instance.Initialize();
+        }
+        return instance;
+    }
+
+    #region Controller
+    
+    #endregion
+
+    #region Model
+    public PlayerDataCollection PlayerDatas;
+
+    #endregion
+
+    private void Initialize()
+    {
+       
+
+    }
+    private void Awake()
+    {
+        GetInstance();
+    }
+}
