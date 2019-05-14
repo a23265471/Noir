8ffff945b1b236715e3d7f208292fc4feabfa020
soraja721547 <soraja721547@gmail.com>
2019-05-14 18:09:48 +0800

    0514 機關

---
+++ Noir/Assets/Script/Component/OrganComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganComponent : MonoBehaviour
+{
+
+    private Animator animator;
+    public string AnimatorTriggerString;
+    public string TriggerTag;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TriggerOrgan()
+    {
+        animator.SetTrigger(AnimatorTriggerString);
+    }
+
+}
---
+++ Noir/Assets/Script/Controller/OrganController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganController : MonoBehaviour
+{
+    public GameObject[] Organ;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < Organ.Length; i++) 
+        {
+            if (other.CompareTag(Organ[i].GetComponent<OrganComponent>().TriggerTag) /*&& other.CompareTag("Player")*/)
+            {
+                Organ[i].GetComponent<OrganComponent>().TriggerOrgan();
+                Debug.Log("h");
+            }
+            
+        }
+
+    }
+
+
+}
